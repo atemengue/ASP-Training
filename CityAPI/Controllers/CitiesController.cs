d2095b0fa077056b0b187f917f1376cb82489958
apple <regisatemengue@outlook.com>
2023-08-04 18:35:07 +0100

    add routing

---
+++ CityAPI/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityAPI.Controllers
+{
+	[ApiController]
+	[Route("api/cities")]
+	public class CitiesController: ControllerBase
+	{
+		//[HttpGet("api/cities")]   
+		public JsonResult GetCities()
+		{
+			return new JsonResult(
+				new List<object> {
+					new { id = 1, Name = "Yaounde"},
+					new { id = 2, Name = "Douala"}
+				});
+			}
+
+    
+    }
+}
+
--- CityAPI/Program.cs
+++ CityAPI/Program.cs
@@ -1,7 +1,8 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,24 +15,33 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 //app.MapControllerRoute(
 //    name: "default",
 //    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Hello word!!");
-});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello word!!");
+//});
 
+app.Run();
