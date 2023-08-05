28cdb3d78b93a59e04eb4ced9212477aadcda784
apple <regisatemengue@outlook.com>
2023-08-05 13:12:13 +0100

    add dumming data and refactor controller

---
+++ CityAPI/CitiesDataStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CityAPI.Models;
+
+namespace CityAPI
+{
+	public class CitiesDataStore
+	{
+		public List<CityDto> Cities { get; set;  }
+
+		public static CitiesDataStore Current { get; } = new CitiesDataStore();
+		public CitiesDataStore()
+		{
+			Cities = new List<CityDto>()
+			{
+				new CityDto()
+				{
+					Id = 1,
+					Name = "New York City",
+					Description = "The one with that big park"
+				},
+				new CityDto()
+				{
+					Id = 2,
+					Name = "Douala Cameroun",
+					Description = "la capitale economique du cameroun"
+				},
+				new CityDto()
+				{
+					Id = 1,
+					Name = "Yaounde",
+					Description = "La capitale politique du cameroun"
+				}
+			};
+		}
+	}
+}
+
--- CityAPI/Controllers/CitiesController.cs
+++ CityAPI/Controllers/CitiesController.cs
@@ -6,17 +6,19 @@ namespace CityAPI.Controllers
 	[Route("api/cities")]
 	public class CitiesController: ControllerBase
 	{
-		//[HttpGet("api/cities")]   
+		//[HttpGet("api/cities")]
+		[HttpGet]
 		public JsonResult GetCities()
 		{
-			return new JsonResult(
-				new List<object> {
-					new { id = 1, Name = "Yaounde"},
-					new { id = 2, Name = "Douala"}
-				});
-			}
+			return new JsonResult(CitiesDataStore.Current);
+		}
 
-    
+		[HttpGet("{id}")]
+		public JsonResult GetCity(int id)
+		{
+			return new JsonResult(
+			CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+		}
     }
-}
+};
 
